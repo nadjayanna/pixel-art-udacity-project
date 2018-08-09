5297d4fc7bb8f90c1878943a736c96cc11003ef5
nadjayanna <nadjayannabr@gmail.com>
2018-08-09 16:54:50 +0200

    make grid with input values

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 $('select[name="colorpicker"]').simplecolorpicker();
+
+function makeGrid(){
+
+	$('#table').empty();
+
+	const rows = $('#gridHeight').val();
+  const cols = $('#gridWidth').val();
+  
+  let table_body = '<table>';
+  
+  for(let i=0; i<rows; i++){
+    table_body+='<tr>';
+    for(let j=0;j<cols;j++){
+      table_body +='<td></td>';
+    }
+    table_body+='</tr>';
+  }
+  table_body+='</table>';
+  
+  $('#table').html(table_body);
+}

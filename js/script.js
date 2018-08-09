54013cc2fa7eb39b132c146b2d30652e34424a20
nadjayanna <nadjayannabr@gmail.com>
2018-08-09 18:56:53 +0200

    add color to the grid

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $('select[name="colorpicker"]').simplecolorpicker();
 
 function makeGrid(){
-
 	$('#table').empty();
 
 	const rows = $('#gridHeight').val();
@@ -12,11 +11,16 @@ function makeGrid(){
   for(let i=0; i<rows; i++){
     table_body+='<tr>';
     for(let j=0;j<cols;j++){
-      table_body +='<td></td>';
+      table_body +="<td id='square'></td>";
     }
     table_body+='</tr>';
   }
   table_body+='</table>';
   
   $('#table').html(table_body);
+  $("#square").on( "click", squareColorHandler );
+}
+
+function squareColorHandler( event ) {
+  $(this).css('background-color', $('select[name="colorpicker"]').val());
 }
